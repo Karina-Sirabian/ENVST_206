7c5cc43908c2688cf7428b9bc60eb5ac912a13b9
Karina <Karina@karinas-air.hamilton.edu>
2020-09-09 09:33:14 -0400

    matrices

--- Activity2.R
+++ Activity2.R
@@ -11,6 +11,26 @@ heights_cm <- heights*100
 heights_cm
 
 ###############################################################################
+################################### Matrices ##################################
+###############################################################################
+# get more info on matrix function
+help(matrix)
+# set up matrix with 2 columns and fill in by rows
+# first argument is the vector of numbers to fill in the matrix
+Mat <- matrix(c(1,2,3,4,5,6), ncol = 2, byrow = TRUE)
+Mat
+# set up matrix that fills in by columns
+# first argument is the vector of numbers to fill in the matrix
+Mat.bycol <- matrix(c(1,2,3,4,5,6), ncol = 2, byrow=FALSE)
+Mat.bycol
+# subset the matrix to look at row 1, column2
+Mat.bycol[1,2]
+# look at all values in row 1
+Mat.bycol[1,]
+# look at all values in column 2
+Mat.bycol[,2]
+
+###############################################################################
 ############## Read in weather station file and create data frame #############
 ###############################################################################
 datW <- read.csv("/Users/russell/Github/ENVST_206/Activity2_Data/noaa2011124.csv")
